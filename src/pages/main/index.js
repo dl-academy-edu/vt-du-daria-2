3df7509b8279f358990284e3ff5977db2843137c
DuDaria <60156592+DuDaria@users.noreply.github.com>
2020-03-23 21:24:17 +0400

    Homework-2 (course-2)
    Выполнила ДЗ№2 Этапа 2

---
+++ src/pages/main/index.js
@@ -0,0 +1,21 @@
+var modal = document.querySelector(".modal-bg_js");
+var buttonOpen = document.querySelector(".slider__link_js");
+var buttonClose = document.querySelector(".modal-window__button_to_close_js");
+var input = document.querySelector(".modal-window__input_js");
+
+buttonOpen.addEventListener("click", function(){
+  modal.classList.remove("modal-bg_close");
+  input.focus();
+});
+
+buttonClose.addEventListener("click",function(){
+  modal.classList.add("modal-bg_close");
+  buttonOpen.focus();
+});
+
+window.addEventListener("keydown", function (event) {
+  if(!modal.classList.contains("modal-bg_close") && event.code==="Escape"){
+    modal.classList.add("modal-bg_close");
+    buttonOpen.focus();
+  }
+});
