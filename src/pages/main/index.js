ecde80faf311c3894a8ed319ba1cc71a32533751
DuDaria <60156592+DuDaria@users.noreply.github.com>
2020-05-21 03:38:43 +0400

    Update Homework-6, 7, 8, 9, 10 (course-2)

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -19,3 +19,17 @@ window.addEventListener("keydown", function (event) {
     buttonOpen.focus();
   }
 });
+
+var buttonMobileHeader = document.querySelector(".header__button-mobile_js");
+var buttonCloseMobileHeader = document.querySelector(".mobile-header__close_js");
+var mobileHeader = document.querySelector(".mobile-header");
+
+//open
+buttonMobileHeader.addEventListener("click",function(){
+  mobileHeader.classList.add("mobile-header_open");
+});
+
+//close
+buttonCloseMobileHeader.addEventListener("click", function(){
+  mobileHeader.classList.remove("mobile-header_open");
+});
